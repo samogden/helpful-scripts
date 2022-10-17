a81245962233bf5f1a83fa7c444780246800d58c
Sam (csumb) <sogden@csumb.edu>
2022-10-17 15:10:36 -0700

    Updating so it doesn't sort columns by question id as this is when the question was made, not where it is in the series of questions

--- grading/canvas/adjustGrades.py
+++ grading/canvas/adjustGrades.py
@@ -5,18 +5,23 @@ from __future__ import annotations
 import json
 import os
 import re
-
+import shutil
 
 import pandas as pd
-import numpy as np
 
 import logging
 logging.basicConfig()
 log = logging.getLogger(__file__)
 log.setLevel(logging.DEBUG)
 
-CSV_FILE = os.path.expanduser("~/classes/CST334 - OS/OS-Ogden-Fall22/exams/exam1/grading/Exam 1 anon.csv")
+CSV_FILE = os.path.expanduser( '/Users/ogde4860/classes/CST334 - OS/OS-Ogden-Fall22/exams/exam1/exam1-score-adjustment/scratch/Exam 1 Quiz Student Analysis Report.csv')
 RUBRIC_FILE = os.path.abspath("keys/cst334-fall2022-exam1.json")
+QUESTIONS_TO_REDO = [
+  "1166835",
+  "1166836",
+  "1166840"
+]
+FEEDBACK_DIR = os.path.expanduser("~/classes/CST334 - OS/OS-Ogden-Fall22/exams/exam1/exam1-score-adjustment/scratch/feedback")
 
 class RubricEntry(object):
   def __init__(self, answer_id, rubric_values, is_extra_credit):
@@ -37,7 +42,11 @@ class Rubric(object):
     
   def __init__(self, rubric_dict: dict):
     self.entries = {
-      key: RubricEntry(key, entry["rubric"], (entry["is_extra_credit"] if "is_extra_credit" in entry else False))
+      key: RubricEntry(
+        key,
+        entry["rubric"],
+        (entry["is_extra_credit"] if "is_extra_credit" in entry else False)
+      )
       for key, entry in rubric_dict.items()
     }
   def __str__(self):
@@ -66,6 +75,8 @@ class Question(object):
       return 0
     else:
       return self.value
+  def is_extra_credit(self):
+    return self.has_rubric and self.rubric.is_extra_credit
 
 class Response(object):
   def __init__(self, question, response, autograde_score):
@@ -102,7 +113,7 @@ class Student(object):
   def get_num_incorrect(self):
     return len(list(filter((lambda r: r.score == 0), self.responses.values())))
   def get_score(self):
-    return sum([q.score for q in self.responses.values()]) / sum([q.question.get_contribution_to_total() for q in self.responses.values()])
+    return 100.0 * sum([q.score for q in self.responses.values()]) / sum([q.question.get_contribution_to_total() for q in self.responses.values()])
   def addResponse(self, response):
     self.responses[response.question.question_id] = response
   def to_list(self, skip_questions=True):
@@ -125,6 +136,26 @@ class Student(object):
     student_list.append(self.get_score())
     return student_list
 
+  def to_dict(self, skip_questions=True):
+    student_dict = {
+      "name" : self.name,
+      "id" : self.id,
+      "sis_id" : self.sis_id,
+      "section" : self.section,
+      "section_id" : self.section_id,
+      "section_sis_id" : self.section_sis_id,
+      "submitted" : self.submitted,
+      "attempt" : self.attempt
+    }
+    if not skip_questions:
+      for response in self.responses.values():
+        student_dict[response.question.question_col] = response.response
+        student_dict[response.question.value_col] = response.score
+    student_dict["n correct"] = self.get_num_correct()
+    student_dict["n incorrect"] = self.get_num_incorrect()
+    student_dict["score"] = self.get_score()
+    return student_dict
+
 
 def get_questions(list_of_columns: list) -> dict[str, Question]:
   questions = {}
@@ -145,7 +176,7 @@ def parse_rubric_file(rubric_file):
   log.debug(rubric)
   return rubric
 
-def getResponses(row, questions) -> list(Response):
+def getResponses(row, questions) -> list[Response]:
   responses = []
   for question in questions.values():
     responses.append(Response(question, row[question.question_col], row[question.value_col]))
@@ -158,6 +189,7 @@ def parse_csv_file(csv_file):
   questions = get_questions(columns)
   students = []
   for index, row in grades_df.iterrows():
+    log.debug(row["name"])
     student = Student(
       row["name"],
       row["id"],
@@ -174,6 +206,31 @@ def parse_csv_file(csv_file):
 
   return columns, students, questions
 
+def getFeedback(questions: dict[str,Question], row):
+  feedback_lines = []
+
+  for i, (q_name, q) in enumerate(questions.items()):
+    if q_name in QUESTIONS_TO_REDO:
+      if row[q.value_col] != q.value:
+        feedback_lines.append(f"You can redo Q{i} ({q.question_id}) for points back!")
+  
+  if len(feedback_lines) != 0:
+    feedback_lines.extend(['', '---', ''])
+
+  #for i, (q_name, q) in enumerate(sorted(questions.items(), key=(lambda s: s[0]))):
+  for i, (q_name, q) in enumerate(questions.items()):
+    feedback_lines.append(f"{'' if (row[q.value_col] == q.value) else '* '}Q{i+1} ({q.question_id}): {row[q.value_col]:.2f} / {q.value}") # : {q_name}") # : {row[q.question_col]}") # : {q.question_col}")
+    if q.is_extra_credit():
+      feedback_lines[-1] += " (Extra Credit)"
+  feedback_lines.append('---')
+  feedback_lines.append(f"Score: {row['score']:0.2f}%")
+  feedback = '\n'.join(feedback_lines)
+  #log.debug(feedback)
+  for idx in row.index:
+    log.debug(f"{idx[:40]} : {row[idx]}")
+  return feedback
+  
+
 def main():
   rubric = parse_rubric_file(RUBRIC_FILE)
   headers, students, questions = parse_csv_file(CSV_FILE)
@@ -185,8 +242,24 @@ def main():
    if question_id in rubric.entries:
      question.add_rubric(rubric.entries[question_id])
      #question.rubric = rubric.entries[question_id]
-  df = pd.DataFrame([s.to_list(False) for s in students], columns = headers)
+  df = pd.DataFrame(
+    [
+      s.to_dict(False)
+      for s in sorted(students, key=(lambda s: s.sis_id))
+    ],
+    columns = headers)
   print(df["score"].describe())
+  df.to_csv("output.csv")
+  df[["score", "id"]].to_csv(os.path.join(os.path.dirname(CSV_FILE), "output.csv"))
+  
+  df["filename"] = df["name"].apply(lambda n: f"{'_'.join(n.split()[-1:] + n.split()[:-1])}.txt" )
+
+  if os.path.exists(FEEDBACK_DIR):
+    shutil.rmtree(FEEDBACK_DIR)
+  os.mkdir(FEEDBACK_DIR)
+  for index, row in df.iterrows():
+    with open(os.path.join(FEEDBACK_DIR, row["filename"]), 'w') as fid:
+      fid.write(getFeedback(questions, row))
 
 
 if __name__ == "__main__":
